aea92e74923430cb8361bbab77233de6a4b720aa
John McCue <jmccue@jmcunx.com>
2022-04-23 17:09:11 -0400

    Fixed err.h for OpenBSD pledge(2)

--- jls.c
+++ jls.c
@@ -29,7 +29,13 @@
 #include <time.h>
 #include <unistd.h>
 
-#ifdef IS_BSD
+#ifdef OpenBSD
+#include <err.h>
+#endif
+#ifdef __FreeBSD_version
+#include <err.h>
+#endif
+#ifdef __NetBSD_Version__
 #include <err.h>
 #endif
 
